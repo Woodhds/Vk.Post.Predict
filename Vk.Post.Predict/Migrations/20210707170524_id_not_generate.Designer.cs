b0692ca940e3b9a4b8eab2595616d44bc5ec28db
borodinsv <borodinsv@npc.ba>
2021-07-07 20:05:59 +0300

    prevent genreate id

--- Vk.Post.Predict/Migrations/20210707170524_id_not_generate.Designer.cs
+++ Vk.Post.Predict/Migrations/20210707170524_id_not_generate.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Vk.Post.Predict.Entities;
 
 namespace Vk.Post.Predict.Migrations
 {
     [DbContext(typeof(DataContext))]
-    partial class DataContextModelSnapshot : ModelSnapshot
+    [Migration("20210707170524_id_not_generate")]
+    partial class id_not_generate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Vk.Post.Predict/Migrations/20210707170524_id_not_generate.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Vk.Post.Predict.Migrations
+{
+    public partial class id_not_generate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

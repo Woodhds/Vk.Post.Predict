1f6c4ba1ff28e235852e4a6909dfe9e6817cbdcb
borodinsv <borodinsv@npc.ba>
2021-07-08 09:44:24 +0300

    serialize

--- Vk.Post.Predict/Controllers/PredictController.cs
+++ Vk.Post.Predict/Controllers/PredictController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Net.Mime;
+using System.Text.Encodings.Web;
 using System.Text.Json;
+using System.Text.Unicode;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -56,7 +58,10 @@ namespace Vk.Post.Predict.Controllers
         public async Task<IActionResult> Get()
         {
             var messages = await _dataContext.Messages.ToListAsync();
-            return File(JsonSerializer.SerializeToUtf8Bytes(messages), MediaTypeNames.Text.Plain, "data.json");
+            return File(JsonSerializer.SerializeToUtf8Bytes(messages, new JsonSerializerOptions
+            {
+                Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+            }), MediaTypeNames.Text.Plain, "data.json");
         }
 
 
